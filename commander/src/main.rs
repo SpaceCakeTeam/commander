e9282bacc60da6d8f54d1a09156fb47736d35afd
Federico Maggi <federico.maggi@mia-platform.eu>
2024-07-03 16:10:36 +0200

    commander server

--- commander/src/main.rs
+++ commander/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, commander!");
+use std::net::ToSocketAddrs;
+use tonic::transport::Server;
+
+mod server;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let server = server::CommanderServer {};
+    Server::builder()
+        .add_service(server::pb::commander_server::CommanderServer::new(server))
+        .serve("[::1]:50051".to_socket_addrs().unwrap().next().unwrap())
+        .await
+        .unwrap();
+
+    Ok(())
 }

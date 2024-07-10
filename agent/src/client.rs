6f24f9a7b35b353861b82af655fa1b434346a83d
Davide Bianchi <bianchidavide12@gmail.com>
2024-07-10 10:38:07 +0200

    feat: implement client

--- agent/src/client.rs
+++ agent/src/client.rs
@@ -11,12 +11,12 @@ use pb::{commander_client::CommanderClient, Message};
 fn message_requests_iter() -> impl Stream<Item = Message> {
   tokio_stream::iter(1..usize::MAX).map(|i| Message {
       name: format!("msg {:02}", i),
-      timestamp: "",
+      timestamp: "".to_string(),
       payload: vec![],
   })
 }
 
-async fn bidirectional_streaming_echo_throttle(client: &mut CommanderClient<Channel>, dur: Duration) {
+pub async fn bidirectional_streaming_echo_throttle(client: &mut CommanderClient<Channel>, dur: Duration) {
   let in_stream = message_requests_iter().throttle(dur);
 
   let response = client
@@ -28,6 +28,6 @@ async fn bidirectional_streaming_echo_throttle(client: &mut CommanderClient<Chan
 
   while let Some(received) = resp_stream.next().await {
       let received = received.unwrap();
-      println!("\treceived message: `{}`", received.message);
+      println!("\treceived message: `{:#?}`", received);
   }
 }
--- agent/src/main.rs
+++ agent/src/main.rs
@@ -1,5 +1,11 @@
 mod client;
 
-fn main() {
-    println!("Hello, agent!");
+use client::pb::commander_client::CommanderClient;
+use std::time::Duration;
+use client::bidirectional_streaming_echo_throttle;
+
+#[tokio::main]
+async fn main() {
+    let mut client = CommanderClient::connect("http://[::1]:50051").await.unwrap();
+    bidirectional_streaming_echo_throttle(&mut client, Duration::from_secs(2)).await;
 }

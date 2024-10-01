5e4d189d5b66993dc378c669856caa760efcf97b
Davide Bianchi <bianchidavide12@gmail.com>
2024-10-01 19:48:15 +0200

    feat: usecase 3 without unit test and with something to change, but it works :)

--- commander/src/http_server.rs
+++ commander/src/http_server.rs
@@ -1,7 +1,7 @@
-use std::{os::macos::raw::stat, sync::Arc};
+use std::sync::Arc;
 
 use axum::{extract::Path, routing::get, Router};
-use messages::{definitions::K8S_GET_VERSION_COMMAND, pb::Message, timenow};
+use messages::{definitions::{K8S_GET_VERSION_COMMAND, K8S_GET_VERSION_EVENT}, pb::Message, timenow};
 
 use crate::commander_state::Commander;
 
@@ -40,7 +40,8 @@ impl CommanderAPI {
 
 // TODO: move somewhere else this handler, possibly in a Controller struct?
 pub async fn version_handler(state: Arc<Commander>, ch_id: String) -> String {
-    let connection = state.get_connection(ch_id);
+    let connection = state.get_connection(ch_id.clone());
+    let now = timenow();
     if connection.is_some() {
         let message = Message{
             name: K8S_GET_VERSION_COMMAND.to_string(),
@@ -48,9 +49,37 @@ pub async fn version_handler(state: Arc<Commander>, ch_id: String) -> String {
             payload: vec![],
         };
         connection.as_ref().clone().unwrap().send(message).await;
-        format!("Version command sent\n")
+
+        println!("|{}|{}| version requested", timenow(), ch_id);
+        let message = event_scanner(state, ch_id.clone(), now).await;
+        println!("|{}|{}| version received", timenow(), ch_id);
+        match message {
+            Some(msg) => {
+                format!("Timestamp: {} Version: {}\n", msg.timestamp, String::from_utf8(msg.payload).unwrap())
+            },
+            None => {
+                format!("Version not found\n")
+            }
+        }
     } else {
         format!("Connection not found\n")
+    }
+}
+
+async fn event_scanner(state: Arc<Commander>, ch_id: String, after_timestamp: u64) -> Option<Message> {
+    loop {
+        let events = state.get_events();
+        let mut message = None;
+        for event in events {
+            if event.channel_id == ch_id && event.message.name == K8S_GET_VERSION_EVENT && event.timestamp > after_timestamp {
+               message = Some(event.message);
+               break;
+            }
+        }
+        if message.is_some() {
+            break message;
+        }
+        tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
     }
 }
 
